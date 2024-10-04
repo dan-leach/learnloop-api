6b53de319bbcb8128008739a022bbc17bcead9fa
Dan Leach <web@danleach.uk>
2024-10-04 16:15:35 +0100

    Added email organisers functionality for feedback/insertSession route. Improved modularity of feedback/insertSession route

--- modules/feedback/routes/insertSession.js
+++ modules/feedback/routes/insertSession.js
@@ -1,19 +1,21 @@
 const config = require("../../../config.json");
 
 /**
- * Inserts a session into the database and returns the session ID and pin.
+ * Inserts a session into the database and sends notification emails to organisers.
  *
- * @async
- * @function insertSession
- * @param {Object} link - The database connection object.
- * @param {Object} data - The sanitized and validated data for the session.
- * @param {boolean} [isSubsession=false] - Flag indicating if the session is a subsession (optional, default is false).
- * @returns {Promise<Object>} Returns an object containing the generated session `id` and `pin`.
- *
- * @example
- * const { id, pin } = await insertSession(link, data);
+ * @param {object} link - Database connection or context to be used for database queries.
+ * @param {object} data - The session data, including details about the session and organisers.
+ * @param {boolean} [isSubsession=false] - Flag indicating whether the session is a subsession of a series.
+ * @param {object} [seriesData={}] - Data from the parent series, if this is a subsession.
+ * @returns {Promise<object>} - The ID of the session and the lead organiser's PIN.
  */
-const insertSession = async (link, data, isSubsession = false, seriesData) => {
+const insertSession = async (
+  link,
+  data,
+  isSubsession = false,
+  seriesData = {}
+) => {
+  // Import utilities for generating IDs, PINs, and hashes
   const {
     createUniqueId,
     createPin,
@@ -21,44 +23,53 @@ const insertSession = async (link, data, isSubsession = false, seriesData) => {
     hashPin,
   } = require("../../utilities/index");
 
-  //generate the session id
+  // Generate a unique session ID
   const id = await createUniqueId(link, "feedback");
-  let leadPin;
+  let leadPin; // Variable to store the lead organiser's PIN
+  const mails = []; // Array to store email details for organisers
 
-  let subsessionIds = [];
+  let subsessionIds = []; // Array to hold IDs of subsessions
   if (!isSubsession) {
-    //insert any subsessions and store array of their ids
-    for (let subsession of data.subsessions) {
-      const { id } = await insertSession(link, subsession, true, data);
-      subsessionIds.push(id);
-      console.error("subsession id: ", id);
-    }
-
-    //generate pin, salt, pinHash for each organiser
+    // Generate PIN, salt, and hashed PIN for each organiser
     for (let organiser of data.organisers) {
       const pin = createPin();
       if (organiser.isLead) {
-        leadPin = pin;
+        leadPin = pin; // Store the lead organiser's PIN to be returned to the client
+        data.leadName = organiser.name; // Store lead organiser's name to be used in emails
       }
       const salt = createSalt();
-
       organiser.pinHash = hashPin(pin, salt);
       organiser.salt = salt;
       organiser.lastSent = null;
       organiser.notifications = organiser.isLead ? data.notifications : true;
 
-      //****email the organiser */
+      // Prepare email data for the organiser
+      mails.push({
+        name: organiser.name,
+        email: organiser.email,
+        pin: pin,
+        isLead: organiser.isLead,
+        canEdit: organiser.canEdit,
+        notifications: organiser.notifications,
+      });
+    }
+
+    // Insert subsessions and collect their IDs to be added to the parent session database row
+    for (let subsession of data.subsessions) {
+      const { id } = await insertSession(link, subsession, true, data);
+      subsessionIds.push(id);
     }
   }
 
   if (isSubsession) {
-    //subsessions take some data from the parent series
+    // If this is a subsession, inherit data from the parent series
     data.date = seriesData.date;
     data.multipleDates = seriesData.multipleDates;
-    data.questions = [];
-    data.certificate = false;
-    data.attendance = false;
-    //create the organisers array
+    data.questions = []; // Subsessions do not have custom questions
+    data.certificate = false; // Subsessions do not directly provide certificates
+    data.attendance = false; // Subsessions do not directly log attendance
+
+    // Generate organiser data for the subsession
     const pin = createPin();
     const salt = createSalt();
     data.organisers = [
@@ -74,9 +85,126 @@ const insertSession = async (link, data, isSubsession = false, seriesData) => {
       },
     ];
 
-    //****email the organiser */
+    // Prepare email data if the subsession organiser has an email
+    if (data.email) {
+      mails.push({
+        name: data.name,
+        email: data.email,
+        pin: pin,
+        isLead: data.organisers[0].isLead,
+        canEdit: data.organisers[0].canEdit,
+        notifications: data.organisers[0].notifications,
+      });
+    }
   }
 
+  // Insert the session into the database
+  await insertSessionIntoDatabase(link, id, data, subsessionIds, isSubsession);
+
+  // Send emails to all organisers
+  for (let mail of mails) {
+    emailOrganiser(
+      data,
+      id,
+      mail.pin,
+      mail.name,
+      mail.email,
+      mail.isLead,
+      mail.canEdit,
+      isSubsession,
+      seriesData
+    );
+  }
+
+  // Return the session ID and the lead organiser's PIN
+  return { id, leadPin };
+};
+
+/**
+ * Sends an email to an organiser with details about the session and their PIN.
+ *
+ * @param {object} data - The session data, including title, organisers, and other relevant information.
+ * @param {string} id - The unique ID of the session.
+ * @param {string} pin - The PIN for the organiser to access the session.
+ * @param {string} name - The name of the organiser.
+ * @param {string} email - The email address of the organiser.
+ * @param {boolean} isLead - Whether the organiser is the lead organiser of the session.
+ * @param {boolean} canEdit - Whether the organiser has editing privileges.
+ * @param {boolean} [isSubsession=false] - Flag indicating whether the email is for a subsession of a series.
+ * @param {object} [seriesData={}] - Data from the parent series, if this is a subsession.
+ * @returns {Promise<boolean>} - Returns `true` if the email was sent successfully, `false` otherwise.
+ */
+const emailOrganiser = (
+  data,
+  id,
+  pin,
+  name,
+  email,
+  isLead,
+  canEdit,
+  isSubsession = false,
+  seriesData = {}
+) => {
+  // Determine the lead organiser's name (in subsession use the parent sessions lead)
+  const leadName = isSubsession ? seriesData.leadName : data.leadName;
+
+  // Define application URLs
+  const appURL = config.client.url;
+  const shortenedAppURL = appURL.replace("https://", ""); // Create a shortened version of the URL to look better on emails
+
+  // Build the body of the email
+  const body = buildMailBody(
+    id,
+    pin,
+    name,
+    isLead,
+    canEdit,
+    leadName,
+    appURL,
+    shortenedAppURL,
+    data,
+    isSubsession,
+    seriesData
+  );
+
+  // Email heading and subject
+  let heading = `Feedback request created`;
+  let subject = `${heading}: ${data.title}`;
+
+  // Import utility function to build HTML structure for the email
+  const { buildMailHTML } = require("../../utilities/index");
+  const html = buildMailHTML(
+    subject,
+    heading,
+    body,
+    isLead,
+    appURL,
+    shortenedAppURL
+  );
+
+  //import the sendMail utility then dispatch the email
+  const { sendMail } = require("../../utilities/index");
+  sendMail(email, subject, html);
+};
+
+/**
+ * Inserts a session and its associated subsessions into the database.
+ *
+ * @param {Object} link - The database connection object used to execute queries.
+ * @param {string} id - The unique identifier for the session.
+ * @param {Object} data - The session data containing details such as name, title, date, and organisers.
+ * @param {Array<string>} subsessionIds - An array of unique identifiers for any subsessions associated with this session.
+ * @param {boolean} isSubsession - A flag indicating whether this is a subsession.
+ * @returns {Promise<boolean>} - Returns a promise that resolves to `true` if the session was successfully inserted, or throws an error if the insertion fails.
+ * @throws {Error} - Throws an error if the database connection fails or if the session insertion fails.
+ */
+const insertSessionIntoDatabase = async (
+  link,
+  id,
+  data,
+  subsessionIds,
+  isSubsession
+) => {
   // Insert the session and subsessions into the database
   if (!link) {
     throw new Error("Database connection failed.");
@@ -96,11 +224,134 @@ const insertSession = async (link, data, isSubsession = false, seriesData) => {
       isSubsession,
       data.attendance,
     ]);
+    return true;
   } catch (error) {
     throw new Error("Failed to insert session into database: " + error.message);
   }
+};
 
-  return { id, leadPin };
+/**
+ * Constructs the body of the feedback request email.
+ *
+ * @param {string} id - The unique identifier for the session.
+ * @param {string} pin - The PIN associated with the session.
+ * @param {string} name - The name of the email recipient.
+ * @param {boolean} isLead - Indicates if the recipient is the lead organiser for the session.
+ * @param {boolean} canEdit - Indicates if the recipient has editing rights for the session.
+ * @param {string} leadName - The name of the lead organiser (if the recipient is not the lead).
+ * @param {string} appURL - The base URL of the application for generating links.
+ * @param {string} shortenedAppURL - The shortened version of the application URL.
+ * @param {Object} data - The session data, including title, date, organisers, and questions.
+ * @param {boolean} isSubsession - Indicates if the session is a subsession.
+ * @param {Object} seriesData - Data related to the parent series, if applicable.
+ * @returns {string} - The constructed HTML body of the email.
+ */
+const buildMailBody = (
+  id,
+  pin,
+  name,
+  isLead,
+  canEdit,
+  leadName,
+  appURL,
+  shortenedAppURL,
+  data,
+  isSubsession,
+  seriesData
+) => {
+  let body = `
+        <p>Hello ${name},<br><br>
+        A feedback request has been successfully created${
+          isLead ? "" : " by " + leadName
+        } on <a href='${appURL}'>LearnLoop</a> for your session '${
+    data.title
+  }' delivered on ${data.multipleDates ? "multiple dates" : data.date}. `;
+
+  if (isSubsession) {
+    body += `This session is part of the series '${seriesData.title}'. `;
+  } else {
+    if (isLead) {
+      body += `You are the lead organsier for this event. This means your access to the session cannot be removed and you have editing rights. `;
+    } else {
+      if (canEdit) {
+        body += `You have been given editing rights for this session. `;
+      } else {
+        body += `You have been given viewing rights for this session. `;
+      }
+    }
+  }
+
+  body += `</p><p>Please keep this email for future reference.</p>
+        <span style='font-size:2em'>Your session ID is <strong>${id}</strong><br>
+        Your session PIN is <strong>${pin}</strong></span><br>
+        Do not share your PIN or this email with attendees. 
+        <a href='${appURL}/feedback/resetPIN/${id}'>Reset your PIN</a>.<br>
+    `;
+
+  // Use join to cleanly concatenate array elements
+  if (data.subsessions && data.subsessions.length) {
+    if (data.subsessions.length === 1) {
+      body += `Feedback will be collected on the session ${data.subsessions[0].title}.<br>`;
+    } else {
+      body += `Feedback will be collected on the sessions: <ul>${data.subsessions
+        .map(
+          (subsession) =>
+            `<li>'${subsession.title}' faciliated by ${subsession.name}</li>`
+        )
+        .join("")}</ul>`;
+    }
+  }
+
+  // Simplify additional questions section
+  if (data.questions && data.questions.length) {
+    body += `The following additional questions will be asked:<ul>
+            ${data.questions
+              .map((question) => `<li>${question.title}</li>`)
+              .join("")}
+            </ul>`;
+  }
+
+  if (canEdit) {
+    body += `<a href='${appURL}/feedback/edit/${id}'>Edit your session</a>. This option is only available <strong>before</strong> feedback has been submitted.`;
+  }
+
+  body += `
+        <p style='font-size:1.5em'>How to direct attendees to the feedback form</p>
+        ${
+          isSubsession
+            ? `
+            <p>The organiser of this session series will share the feedback link for the whole series with attendees.</p>
+        `
+            : `
+            You can share the direct link: <a href='${appURL}/${id}'>${shortenedAppURL}/${id}</a><br>
+            Or, ask them to go to <a href='${appURL}'>${shortenedAppURL}</a> and enter the session ID.<br>
+            Or, <a href='${appURL}/feedback/instructions/${id}'>show a page with instructions on how to reach the feedback form</a> including a QR code for your attendees to scan.<br>
+            ${
+              data.certificate
+                ? "<br>Don't forget to let your attendees know that they'll be able to download a certificate of attendance after completing feedback."
+                : ""
+            }
+        `
+        }
+    `;
+
+  body += `
+        <p style='font-size:1.5em'>View your feedback</p>
+        <p>Go to <a href='${appURL}/feedback/view/${id}'>${shortenedAppURL}/feedback/view/${id}</a> and enter your PIN to retrieve submitted feedback.<br>
+        ${
+          data.notifications
+            ? "Email notification of feedback submissions is <strong>enabled</strong>. "
+            : "Email notification of feedback submissions is <strong>disabled</strong>. "
+        }
+        <a href='${appURL}/feedback/notifications/${id}'>Update your notification preferences</a>.<br>
+        ${
+          !isSubsession && data.attendance
+            ? `The attendance register is <strong>enabled</strong>. <a href='${appURL}/feedback/attendance/${id}'>View attendance register</a>.<br>`
+            : ""
+        }
+    `;
+
+  return body;
 };
 
 module.exports = { insertSession };
--- modules/utilities/index.js
+++ modules/utilities/index.js
@@ -121,10 +121,217 @@ const pinIsValid = (pin, salt, pinHash, adminPinHash) => {
   return pinHash === hash;
 };
 
+/**
+ * Generates the footer content for an email, optionally including a development mode notice and an invitation to use LearnLoop.
+ *
+ * @param {boolean} includeInvite - Whether to include an invitation to use LearnLoop.
+ * @param {string} appURL - The full URL for LearnLoop.
+ * @param {string} shortenedAppURL - The shortened version of the LearnLoop URL for display.
+ * @returns {string} - The generated HTML string for the footer content.
+ */
+const addMailFooter = (includeInvite, appURL, shortenedAppURL) => {
+  let footer = "";
+
+  // Add dev mode warning if applicable
+  if (config.devMode) {
+    footer += `
+      <p>
+        This session uses the development version of LearnLoop and may include experimental features which might not be supported long-term. Please report any bugs or other feedback to <a href='mailto:${config.email}'>${config.email}</a>.
+      </p>`;
+  }
+
+  // Optionally include an invitation to use LearnLoop
+  if (includeInvite) {
+    footer += `
+      <p>
+        You can request feedback for your own sessions using LearnLoop. Visit <a href='${appURL}'>${shortenedAppURL}</a> to get started!
+      </p>`;
+  }
+
+  return footer;
+};
+
+/**
+ * Builds the HTML structure for an email, including the header, body, and footer.
+ *
+ * @param {string} subject - The subject of the email, used in the title tag.
+ * @param {string} heading - The main heading text displayed in the email body.
+ * @param {string} body - The HTML content for the body of the email.
+ * @param {boolean} isLead - Determines whether the recipient is a lead, influencing the footer content.
+ * @param {string} appURL - The full URL of the application, used for links in the email.
+ * @param {string} shortenedAppURL - A shortened version of the app URL, used in the footer invitation link.
+ */
+
+const buildMailHTML = (
+  subject,
+  heading,
+  body,
+  isLead,
+  appURL,
+  shortenedAppURL
+) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${subject}</title>
+      <style>
+        /* General email resets */
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+        }
+        table {
+          border-spacing: 0;
+        }
+        img {
+          display: block;
+          border: 0;
+        }
+        td {
+          padding: 0;
+        }
+        /* Container styling */
+        .email-container {
+          width: 100%;
+          max-width: 600px;
+          margin: 0 auto;
+          background-color: #ffffff;
+          border-radius: 10px;
+          box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        }
+        .email-header {
+          background-color: #17a2b8;
+          padding: 20px;
+          text-align: center;
+          color: #ffffff;
+        }
+        .email-body {
+          padding: 20px;
+          color: #333333;
+          line-height: 1.6;
+        }
+        .email-footer {
+          padding: 20px;
+          text-align: center;
+          font-size: 12px;
+          color: #888888;
+        }
+        a {
+          color: #17a2b8;
+        }
+      </style>
+    </head>
+    <body>
+      <!-- Start Email Container -->
+      <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="background-color: #f4f4f4; padding: 20px;">
+        <tr>
+          <td>
+            <!-- Email Content Area -->
+            <table class="email-container" cellpadding="0" cellspacing="0" role="presentation">
+              <!-- Email Header -->
+              <tr>
+                <td class="email-header">
+                  <img src='cid:logo' alt='LearnLoop Logo' height='50'>
+                </td>
+              </tr>
+
+              <!-- Email Body -->
+              <tr>
+                <td class="email-body">
+                  <h2>${heading}</h2>
+                  ${body}
+                  Kind regards,<br>
+                  <strong><a href='${appURL}'>LearnLoop</a></strong><br>
+                </td>
+              </tr>
+
+              <!-- Email Footer -->
+              <tr>
+                <td class="email-footer">
+                  <p>&copy; ${new Date().getFullYear()} LearnLoop</p>
+                  ${addMailFooter(!isLead, appURL, shortenedAppURL)}
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
+  </html>
+`;
+};
+
+/**
+ * Sends an email using Nodemailer.
+ *
+ * @param {string} email - The recipient's email address.
+ * @param {string} subject - The subject line of the email.
+ * @param {string} html - The HTML content of the email.
+ * @returns {Promise<boolean>} - Returns a promise that resolves to `true` if the email was sent successfully, or `false` if an error occurred.
+ */
+const sendMail = (email, subject, html) => {
+  const nodemailer = require("nodemailer");
+
+  // Load DKIM and email credentials
+  const keys = require("../../private/keys.json");
+
+  // Configure the email transporter using Nodemailer
+  const transporter = nodemailer.createTransport({
+    host: "mail.learnloop.co.uk",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+      user: "noreply@learnloop.co.uk",
+      pass: keys.emailKey,
+    },
+    dkim: {
+      domainName: "learnloop.co.uk",
+      privateKey: keys.emailDkimPrivateKey,
+    },
+  });
+
+  // Import node path module used to build path to logo
+  const path = require("path");
+
+  // Email options including the recipient, subject, HTML content, and attachments
+  const mailOptions = {
+    from: "noreply@learnloop.co.uk",
+    to: email,
+    subject: subject,
+    html: html,
+    attachments: [
+      {
+        filename: "logo.png", // The logo file name to include in the email
+        path: path.resolve(__dirname, "./logo.png"), // The full path to the logo image
+        cid: "logo", // Content ID to reference the logo in the HTML content
+      },
+    ],
+  };
+
+  // Send the email using the transporter
+  return transporter
+    .sendMail(mailOptions)
+    .then((info) => {
+      console.log("Email sent: " + info.response); // Log successful email response
+      return true; // Return success status
+    })
+    .catch((error) => {
+      console.error("sendMail failed", error); // Log any errors that occur during email sending
+      return false; // Return failure status
+    });
+};
+
 module.exports = {
   createUniqueId,
   createPin,
   createSalt,
   hashPin,
   pinIsValid,
+  buildMailHTML,
+  sendMail,
 };
