67424561d80e28739f9fd7ed17453050744a1e3b
Dan Leach <web@danleach.uk>
2025-01-05 23:01:06 +0000

    Removed temp db harmonisation code. Added Interaction expression of interest route

--- modules/interaction/index.js
+++ modules/interaction/index.js
@@ -1,8 +1,67 @@
+/**
+ * @module interaction
+ * @memberof module:LearnLoopAPI
+ * @summary Module for handling interaction-related routes in the application.
+ *
+ * @requires express
+ * @requires router
+ * @requires express-validator
+ * @requires ./validate Rulesets and validation function for each route
+ *
+ * @exports router Object containing the different routes available in the feedback module
+ */
+
 const express = require("express");
 const router = express.Router();
+const { matchedData } = require("express-validator");
+const validate = require("./validate");
+const { dbConfig, openDbConnection } = require("../utilities/dbUtilities");
+const { handleError } = require("../utilities/routeUtilities");
 
-router.post("/insertSession", (req, res) => {
-  res.json("3");
-});
+/**
+ * @async
+ * @route POST /feedback/interest
+ * @memberof module:interest
+ * @summary Inserts an interested party's email into the db table.
+ *
+ * @requires ./validate - Module for defining validation rules and sanitizing request data.
+ */
+router.post(
+  "/interest",
+  validate.interestRules, // Middleware for validating data
+  validate.validateRequest, // Middleware for validating the request based on the rules
+  async (req, res) => {
+    let link; // Database connection variable
+    try {
+      // Get the validated and sanitized data from the request
+      const { email } = matchedData(req);
+
+      // Open a connection to the database
+      link = await openDbConnection(dbConfig);
+
+      await link.execute(
+        `INSERT INTO tbl_interaction_interest (email) VALUES (?)`,
+        [email]
+      );
+
+      // Respond with the session ID and lead organiser pin
+      res.json({
+        message:
+          "Thanks for your interest. I'll let you know when LearnLoop Interaction is available or needs beta-testers.",
+      });
+    } catch (error) {
+      handleError(
+        error,
+        error.statusCode,
+        "interaction/interest",
+        "Failed to register email on interest list",
+        res
+      );
+    } finally {
+      // Close the database connection if it was opened
+      if (link) await link.end();
+    }
+  }
+);
 
 module.exports = router;
---
+++ modules/interaction/validate.js
@@ -0,0 +1,51 @@
+/**
+ * @module validate
+ * @memberof module:interaction
+ * @summary Specifies validation rules and checks requests against these for interaction module routes.
+ * @description
+ * This module defines validation rules for various routes related to interaction session management.
+ * It utilizes the `express-validator` library to enforce data integrity and ensure that all required
+ * fields meet specified criteria before being processed by the application.
+ *
+ * @requires express-validator - Validation library for performing data validation and sanitization
+ * @requires ../utilities/routeUtilities Error handling
+ *
+ * @exports interestRules - Ruleset for the interest route
+ */
+
+const { check, validationResult } = require("express-validator");
+const { handleError } = require("../utilities/routeUtilities");
+
+/**
+ * Validation rules for the interest route
+ * @type {array}
+ */
+const interestRules = [
+  check("email")
+    .isEmail()
+    .withMessage("Email field must be a valid email address format."),
+];
+
+// Middleware function to validate the request
+const validateRequest = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  } catch (error) {
+    handleError(
+      error,
+      500,
+      "validateRequest",
+      "Failed to validate request",
+      res
+    );
+  }
+};
+
+module.exports = {
+  interestRules,
+  validateRequest,
+};
